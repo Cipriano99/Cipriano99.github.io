0d60e2873498ce431ad4e0758674d430a81ecafd
Samuel Cipriano <samuelcipriano99@gmail.com>
2020-06-15 15:08:36 -0300

    responsiveness updates

--- ciprianojs/cipriano.js
+++ ciprianojs/cipriano.js
@@ -4,6 +4,9 @@ const barraNavegacao = document.querySelector('.barra-navegacao');
 const menu = document.querySelector('.menu');
 
 menu.addEventListener('click', () => {
+
+  document.body.style.overflow = show ? 'hidden' : 'initial'
+
   barraNavegacao.classList.toggle('on', show);
   show = !show;
 })
